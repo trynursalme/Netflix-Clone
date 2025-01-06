c68f758652c2be130020d2b84517b0d327924fd3
Nurselam  Hussen - ZOA Ethiopia <n.hussen@zoa.ngo>
2025-01-06 12:41:36 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
