ef02a61ad9901b39901996357d204b130fe91829
Nurselam  Hussen - ZOA Ethiopia <n.hussen@zoa.ngo>
2025-01-14 12:05:48 +0300

    adding Banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <Home/>
     </div>
   );
 }
--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+            <Header/>
+            <Banner/>
+            <Footer/>
+    </div>
   )
 }
 
