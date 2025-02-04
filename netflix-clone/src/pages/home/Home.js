ac0068963fefff4ec48043399014db43288436ab
Nurselam  Hussen - ZOA Ethiopia <n.hussen@zoa.ngo>
2025-02-04 12:30:28 +0300

    working on the row

--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
+import RowList from '../../components/Rows/RowList/RowList';
+import Row from '../../components/Rows/Row/Row';
 
 const Home = () => {
   return (
     <div>
             <Header/>
             <Banner/>
+            <Row/>
+            <RowList/>
             <Footer/>
     </div>
   )
